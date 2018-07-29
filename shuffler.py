007a49cb1ad458c4cef8cb955c33abeba7e83180
Ethan Ryan <erya7975@uni.sydney.edu.au>
2018-07-29 11:29:52 +1000

    Shuffler uses weights from input file

--- shuffler.py
+++ shuffler.py
@@ -1,10 +1,26 @@
 import spotipy
 import spotipyFuncs as msp
 import numpy as np
+import pandas as pd
 
-n_songs = 10  # Number of songs to play
-new_ids = []  # List for song URIs
+n_songs = 10
 
+# Load spreadsheet
+weightings=pd.read_excel('weightings.xlsx')
+
+# Calculate total of weightings for normalisation
+total_weight = weightings['Weighting'].sum()
+# Normalise weightings to probability (p=weighting/total_weight)
+probs = weightings['Weighting']/total_weight
+
+ids = list(weightings['URI'])
+new_ids = []
+
+# Sample n_songs without replacement with probability probs
+for idx in range(n_songs):
+    new_ids.append(np.random.choice(ids, p=probs))
+
+# loginto spotify
 # Get access token
 token = msp.get_token('hrkp',
                       scope='user-modify-playback-state playlist-read-private\
@@ -14,28 +30,6 @@ token = msp.get_token('hrkp',
 playlist_uri = 'spotify:user:hrkp:playlist:097gO1u0aaAjH0u2lX1Ylg'
 
 sp = spotipy.Spotify(auth=token)
-
-shuffler_playlist = sp.user_playlist('hrkp', playlist_id=playlist_uri)[
-                                     'tracks']['items']
-
-weight_dict = {}
-
-
-# Ask user for weight of songs in shuffler_playlist
-for track in shuffler_playlist:
-    weight = input('Weighting of {}: '.format(track['track']['name']))
-    id = track['track']['uri']
-    weight_dict[id] = weight
-
-# Create lists of song URIs and normalised weightings
-ids = list(weight_dict.keys())
-probs = np.array(list(weight_dict.values()))
-probs = probs.astype(float)
-probs = 1/sum(probs)*probs
-
-# Choose n_songs random songs with PDF from normalised weightings
-for idx in range(n_songs):
-    new_ids.append(np.random.choice(ids, p=probs))
 
 # Play shuffled songs
 sp.start_playback(uris=new_ids)
